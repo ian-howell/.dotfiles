c693f62cb7aa007ae2a6ba7038aa70cf945d6778
Ian Howell <ian.howell0@gmail.com>
2019-01-19 11:17:52 -0600

    Clean up essentials builder

--- src/essentials.sh
+++ src/essentials.sh
@@ -1,31 +1,23 @@
 set -x
 
 echo "Setting up essentials"
-sudo apt-get -qq -y update
-sudo apt-get -qq -y install \
-  libncurses5-dev \
-  libgnome2-dev \
-  libgnomeui-dev \
-  libgtk2.0-dev \
-  libatk1.0-dev \
-  libbonoboui2-dev \
-  libcairo2-dev \
-  libx11-dev \
-  libxpm-dev \
-  libxt-dev \
-  python-dev \
-  python3-dev \
-  ruby-dev \
-  lua5.1 \
-  lua5.1-dev \
-  libperl-dev \
-  git \
-  curl \
-  make \
+sudo apt-get -y update
+
+# The following are library-type things. They're basically dependencies
+sudo apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
+                libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev \
+                libx11-dev libxpm-dev libxt-dev python-dev python3-dev \
+                ruby-dev lua5.1 lua5.1-dev libperl-dev
+
+# These are my programs
+sudo apt-get install -y
   ctags \
+  curl \
+  git \
+  make \
+  rxvt-unicode-256color \
   silversearcher-ag \
-  tree \
-  xclip \
   tmux \
-  rxvt-unicode-256color
+  tree \
+  xclip
 set +x
