312f0b0069eae082b9a0c827a611faefba050ea2
Ian Howell <ian.howell0@gmail.com>
2019-01-19 17:25:12 -0600

    Remove the useless stuff

--- TODO.md
+++
@@ -1,5 +0,0 @@
-* Need to install fzf
-* Need to install fd
-* Need to install ag
-
-* Move rc functions to their own file so they can be run in both bash and zsh
--- bin/touch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ih616h/backup
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -cp etc:authz-batch-2.0.18-jar-with-dependencies.jar com.att.authz.Batch CacheTch
