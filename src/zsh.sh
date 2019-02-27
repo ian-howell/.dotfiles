0355519d9e134c86dffb9011f934b6e2f440f939
Ian Howell <ian.howell0@gmail.com>
2019-02-27 08:41:29 -0600

    Get rid of oh-my-zsh
    The only thing I haven't stolen yet is completion

--- src/zsh.sh
+++ src/zsh.sh
@@ -1,26 +1,9 @@
 set -x
 install_zsh() {
-  if [ ! -n "$ZSH" ]; then
-    ZSH=~/.oh-my-zsh
-  fi
-
-  # Prevent the cloned repository from having insecure permissions. Failing to do
-  # so causes compinit() calls to fail with "command not found: compdef" errors
-  # for users with insecure umasks (e.g., "002", allowing group writability). Note
-  # that this will be ignored under Cygwin by default, as Windows ACLs take
-  # precedence over umasks except for filesystems mounted with option "noacl".
-  umask g-w,o-w
-
   zsh_exists=$(command -v zsh)
   if [ -z "$zsh_exists" ]; then
       sudo apt-get -qq -y install zsh
   fi
-
-  echo "Cloning Oh My Zsh..."
-  env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$ZSH" || {
-    echo "Error: git clone of oh-my-zsh repo failed"
-    return
-  }
 
   # If this user's login shell is not already "zsh", attempt to switch.
   TEST_CURRENT_SHELL=$(basename "$SHELL")
