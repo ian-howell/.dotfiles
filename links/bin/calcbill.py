c1f548efc478c78313dcb85fede5b4b765192f84
Ian Howell <ian.howell0@gmail.com>
2021-10-05 10:49:52 -0500

    Fix calcbill.py to work with new ATT format
    This also includes several QoL changes to the script

--- links/bin/calcbill.py
+++ links/bin/calcbill.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-""" How to use this script:
-
+"""
+How to use this script:
     1. Go to att.com/my and sign in
-    2. Click the blue plus bubble under "My bills"
-    3. Click "See my bill"
-    4. Under the "Bill total section, click the "Print" link
-    5. On this page, select all the text (ctrl-a)
+    2. Click "See charges and Payments"
+    3. Click "Show bill details"
+    5. Select all the text (ctrl-a)
     6. Run the script, and paste all of the text into the console
     7. Hammer on the enter key a few times to scroll, then hit ctrl-d
 """
@@ -23,41 +22,38 @@ def main():
 
 
 def get_data_from_stdin():
-    saving = False
     lines = sys.stdin.readlines()
-    i = 0
     results = {
             "mickey_group": {},
             "pam_group": {},
             "ian_group": {},
+            "monthly": 0,
             }
-    while i < len(lines):
-        line = lines[i]
-        if line == "10 devices\n":
-            saving = True
+    for i, line in enumerate(lines):
+        # explicitly passing a space forces the resulting list to have len >= 1
+        name = line.split(" ", 1)[0]
+        if name == "AT&T":
             results['monthly'] = parse_money(lines[i+1])
-        if saving:
-            name = line.split(" ", 1)[0]
-            if name in mickey_group:
-                results["mickey_group"][line.strip()] = parse_money(lines[i+1])
-            if name in pam_group:
-                results["pam_group"][line.strip()] = parse_money(lines[i+1])
-            if name in ian_group:
-                results["ian_group"][line.strip()] = parse_money(lines[i+1])
-        i += 1
+        elif name in mickey_group:
+            results["mickey_group"][line] = parse_money(lines[i+1])
+        elif name in pam_group:
+            results["pam_group"][line] = parse_money(lines[i+1])
+        elif name in ian_group:
+            results["ian_group"][line] = parse_money(lines[i+1])
     return results
 
 
 def parse_money(money_str):
-    return float(money_str[1:-1])
+    return float(money_str.lstrip("$"))
 
 
 def print_report(report):
     total = report['monthly']
     for group_name in sorted(report):
-        if not group_name.endswith('group'):
+        if group_name == 'monthly':
             continue
         group = report[group_name]
+        # strip off the string "_group" from the group name and capitalize the group owner name
         group_owner = group_name[:-len('_group')].title()
         group_total = total_for_group(group)
         total += group_total
