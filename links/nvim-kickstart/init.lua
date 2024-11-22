0a72d02d301b139cc78424231cd49e75e9ca4df1
Ian Howell <ian.howell0@gmail.com>
2024-11-22 00:23:32 +0000

    Fix a tmux navigation keybinding

--- links/nvim-kickstart/init.lua
+++ links/nvim-kickstart/init.lua
@@ -948,7 +948,7 @@ require('lazy').setup({
     vim.keymap.set('n', '<c-h>', ':TmuxNavigateLeft<cr>', { silent = true }),
     vim.keymap.set('n', '<c-j>', ':TmuxNavigateDown<cr>', { silent = true }),
     vim.keymap.set('n', '<c-k>', ':TmuxNavigateUp<cr>', { silent = true }),
-    vim.keymap.set('n', 'c-l>', ':TmuxNavigateRight<cr>', { silent = true }),
+    vim.keymap.set('n', '<c-l>', ':TmuxNavigateRight<cr>', { silent = true }),
   },
   {
     -- TODO: Look into this one, it looks cool.
