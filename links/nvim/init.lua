39c6ba3af5396e7862feda3332795c11d4e5b716
Ian Howell <ian.howell0@gmail.com>
2024-11-24 00:17:30 +0000

    Fix inactive window colors in vim

--- links/nvim/init.lua
+++ links/nvim/init.lua
@@ -152,6 +152,41 @@ vim.opt_local.cursorline = true
 vim.opt_local.cursorcolumn = true
 vim.opt_local.colorcolumn = '80'
 
+-- NOTE: If colors are funky, don't forget to test with the true-color-24-test
+vim.opt.termguicolors = true
+--
+-- Background colors for active vs inactive windows
+vim.cmd.hi 'link ActiveWindow Normal'
+
+-- InactiveWindow is the background color for tokyonight
+vim.cmd.hi 'InactiveWindow guibg=#1a1b26'
+
+-- Normally, this would be good enough (and it's still required for startup with
+-- multiple splits)
+vim.opt.winhighlight = 'Normal:ActiveWindow,NormalNC:InactiveWindow'
+-- But since vim always has an "active" window (even in an inactive tmux pane),
+-- I need to use autocmds to change the highlight groups based on focus.
+vim.api.nvim_create_autocmd({ 'BufLeave', 'WinLeave', 'FocusLost' }, {
+  desc = 'Dim the window when it loses focus',
+  group = vim.api.nvim_create_augroup('focus-leave', { clear = true }),
+  callback = function()
+    vim.opt_local.cursorline = false
+    vim.opt_local.cursorcolumn = false
+    vim.opt_local.colorcolumn = ''
+    vim.opt_local.winhighlight = 'Normal:InactiveWindow,NormalNC:InactiveWindow'
+  end,
+})
+vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter', 'FocusGained' }, {
+  desc = 'Highlight the window when it gains focus',
+  group = vim.api.nvim_create_augroup('focus-enter', { clear = true }),
+  callback = function()
+    vim.opt_local.cursorline = true
+    vim.opt_local.cursorcolumn = true
+    vim.opt_local.colorcolumn = '80'
+    vim.opt_local.winhighlight = 'Normal:ActiveWindow,NormalNC:InactiveWindow'
+  end,
+})
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
