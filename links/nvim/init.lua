2f0016db49f0bed15db4d3310e729d10c3c38c25
Ian Howell <ian.howell0@gmail.com>
2024-11-24 18:46:42 +0000

    Clean up some of the comments in the init.lua file

--- links/nvim/init.lua
+++ links/nvim/init.lua
@@ -1,92 +1,9 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
+-- Lua resources:
+--  - https://learnxinyminutes.com/docs/lua/
+--  - :help lua-guide (or HTML version): https://neovim.io/doc/user/lua-guide.html
 
 -- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -131,8 +48,7 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
+--  See `:help 'list'` and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
 
@@ -146,18 +62,17 @@ vim.opt.grepprg = 'rg --vimgrep --hidden -i -S'
 
 -- NOTE: If colors are funky, don't forget to test with the true-color-24-test
 vim.opt.termguicolors = true
---
+
 -- Background colors for active vs inactive windows
 vim.cmd.hi 'link ActiveWindow Normal'
 
 -- InactiveWindow is the background color for tokyonight
 vim.cmd.hi 'InactiveWindow guibg=#1a1b26'
 
--- Normally, this would be good enough (and it's still required for startup with
--- multiple splits)
+-- Normally, this would be good enough (and it's still required for startup with multiple splits)...
 vim.opt.winhighlight = 'Normal:ActiveWindow,NormalNC:InactiveWindow'
--- But since vim always has an "active" window (even in an inactive tmux pane),
--- I need to use autocmds to change the highlight groups based on focus.
+-- ... But since vim always has an "active" window (even in an inactive tmux pane), I need to use autocmds to
+-- change the highlight groups based on focus.
 vim.api.nvim_create_autocmd({ 'BufLeave', 'WinLeave', 'FocusLost' }, {
   desc = 'Dim the window when it loses focus',
   group = vim.api.nvim_create_augroup('focus-leave', { clear = true }),
@@ -165,6 +80,8 @@ vim.api.nvim_create_autocmd({ 'BufLeave', 'WinLeave', 'FocusLost' }, {
     vim.opt_local.cursorline = false
     vim.opt_local.cursorcolumn = false
     vim.opt_local.colorcolumn = ''
+    -- If I've left the window for *any* reason (whether it's to switch buffers or to switch tmux panes),
+    -- I want the window to be dimmed.
     vim.opt_local.winhighlight = 'Normal:InactiveWindow,NormalNC:InactiveWindow'
   end,
 })
@@ -186,20 +103,19 @@ vim.opt.winblend = 10
 vim.opt.textwidth = 110
 
 -- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+-- See `:help vim.keymap.set()`
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
+-- Toggle highlights
+-- TODO: Should this start with 't' to match the other toggles?
 vim.keymap.set('n', '<leader>hl', '<cmd>nohlsearch<CR>')
 
--- Keybinds to make split navigation easier.
---  Easy access to :split and :vsplit. Use '\' instead of '|' because of Shift
+-- Easy access to :split and :vsplit. Use '\' instead of '|' because of Shift.
 vim.keymap.set('n', '-', ':split<CR>', { desc = 'Split window horizontally' })
 vim.keymap.set('n', '\\', ':vsplit<CR>', { desc = 'Split window vertically' })
 
---  Use CTRL+<hjkl> to switch between windows
+-- Use CTRL+<hjkl> to switch between windows
 --
---  See `:help wincmd` for a list of all window commands
+-- See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
@@ -254,8 +170,7 @@ end, { desc = 'git diff' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
+-- Highlight the yanked text
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
@@ -265,26 +180,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- When editing a file, always jump to the last known cursor position (if it's
--- valid). Ignores commit messages (it's likely a different one than last time).
+-- When editing a file, always jump to the last known cursor position (if it's valid).
+-- Ignores commit messages (it's likely a different one than last time).
 RecallLastPosition = function()
   if vim.fn.line '\'"' < 1 then
-    -- We've never opened this file before
-    return
+    return -- We've never opened this file before
   end
 
   if vim.fn.line '\'"' > vim.fn.line '$' then
-    -- The file has shrunk since we last edited it
-    return
+    return -- The file has shrunk since we last edited it
   end
 
   if vim.bo.ft == 'gitcommit' then
-    -- We don't want to do this for git commit messages
-    return
+    return -- We don't want to do this for git commit messages
   end
 
-  -- Jump to the last known cursor position
-  vim.cmd 'normal! g`"'
+  vim.cmd 'normal! g`"' -- Jump to the last known cursor position
 end
 
 vim.api.nvim_create_autocmd('BufReadPost', {
@@ -294,7 +205,7 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -306,17 +217,9 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
 require('lazy').setup({
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
