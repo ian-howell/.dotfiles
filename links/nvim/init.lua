b8f5d7072ab320d78eeacf8d43a3bf7a48fb15d4
Ian Howell <ian.howell0@gmail.com>
2024-11-24 17:16:18 +0000

    Prevent the sign column from opening when there are diagnostics

--- links/nvim/init.lua
+++ links/nvim/init.lua
@@ -719,10 +719,11 @@ require('lazy').setup({
 
       -- Change diagnostic symbols in the sign column (gutter)
       if vim.g.have_nerd_font then
-        local signs = { Error = '', Warn = '', Hint = '', Info = '' }
-        for type, icon in pairs(signs) do
+        local types = { 'Error', 'Warn', 'Hint', 'Info' }
+        for _, type in ipairs(types) do
           local hl = 'DiagnosticSign' .. type
-          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+          -- Specifically set the text icon to be empty - I don't want to open the sign column
+          vim.fn.sign_define(hl, { text = "", texthl = hl, numhl = hl })
         end
       end
 
