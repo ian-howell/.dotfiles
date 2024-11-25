ea2c563fe6d984fa676dc4009815b068ccb8e049
Ian Howell <ian.howell0@gmail.com>
2024-11-25 00:53:53 +0000

    Smooth cursoter movement, scrolling, and window resizing

--- links/nvim/init.lua
+++ links/nvim/init.lua
@@ -863,30 +863,49 @@ require('lazy').setup({
       -- animate is more feature rich, and includes things like animated
       -- scrolling and window resizing. I'd like to use both, but prefer
       -- smear-cursor for moving the cursor around.
-      require('mini.animate').setup()
+      require('mini.animate').setup({
+        -- Prefer Smear
+        cursor = { enable = false, },
+        -- Prefer Neoscroll
+        scroll = { enable = false, },
+        -- These are for floating windows, but they just seem to slow things down? I'm not really sure what
+        -- they're supposed to do.
+        open = { enable = false, },
+        close = { enable = false, },
+      })
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
 
-  -- { -- Smear cursor when moving between buffers
-  -- TODO: Fix this - it's really cool, but it's throwing errors... :(
-  --
-  --   'sphamba/smear-cursor.nvim',
-  --
-  --   opts = {
-  --     -- Smear cursor when switching buffers
-  --     smear_between_buffers = true,
-  --
-  --     -- Use floating windows to display smears outside buffers.
-  --     -- May have performance issues with other plugins.
-  --     use_floating_windows = true,
-  --
-  --     -- Attempt to hide the real cursor when smearing.
-  --     hide_target_hack = true,
-  --   },
-  -- },
+  { -- Smear cursor when moving between buffers
+    'sphamba/smear-cursor.nvim',
+
+    opts = {
+      -- Smear cursor when switching buffers
+      smear_between_buffers = true,
+
+      -- Use floating windows to display smears outside buffers.
+      -- May have performance issues with other plugins.
+      use_floating_windows = true,
+
+      -- Attempt to hide the real cursor when smearing.
+      hide_target_hack = true,
+    },
+  },
+  { -- Smooth scrolling
+    "karb94/neoscroll.nvim",
+    config = function()
+      require('neoscroll').setup({
+        mappings = {
+          '<C-u>', '<C-d>',
+          '<C-b>', '<C-f>',
+          'zt', 'zz', 'zb',
+        },
+      })
+    end
+  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
