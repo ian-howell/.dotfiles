d97458c60a31c4f9edb242e3bdbbf02504c0a325
Ian Howell <ian.howell0@gmail.com>
2024-11-28 12:08:53 -0600

    Extract lazy bootstrap code to its own file

--- links/nvim/init.lua
+++ links/nvim/init.lua
@@ -224,16 +224,7 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
--- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+require 'lazy-bootstrap' -- ~/.config/nvim/lua/lazy-bootstrap.lua
 
 -- [[ Configure and install plugins ]]
 require('lazy').setup({
