87b290ba458ed86124568045c877a745ce45d746
Ian Howell <ian.howell0@gmail.com>
2024-11-29 17:25:57 -0600

    Remove some duplicate keymaps

--- links/nvim/lua/keymaps.lua
+++ links/nvim/lua/keymaps.lua
@@ -8,14 +8,6 @@ vim.keymap.set('n', '<leader>th', '<cmd>set hlsearch!<CR>')
 vim.keymap.set('n', '-', ':split<CR>', { desc = 'Split window horizontally' })
 vim.keymap.set('n', '\\', ':vsplit<CR>', { desc = 'Split window vertically' })
 
--- Use CTRL+<hjkl> to switch between windows
---
--- See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 -- Quickfix Mappings
 -- TODO: Is this the right way to define a function?
 local toggleQuickFix = function()
--- links/nvim/lua/plugins/tmux-navigator.lua
+++ links/nvim/lua/plugins/tmux-navigator.lua
@@ -1,10 +1,23 @@
 return {
   { -- TMUX Navigation
     'christoomey/vim-tmux-navigator',
-    vim.keymap.set('n', '<c-h>', ':TmuxNavigateLeft<cr>', { silent = true }),
-    vim.keymap.set('n', '<c-j>', ':TmuxNavigateDown<cr>', { silent = true }),
-    vim.keymap.set('n', '<c-k>', ':TmuxNavigateUp<cr>', { silent = true }),
-    vim.keymap.set('n', '<c-l>', ':TmuxNavigateRight<cr>', { silent = true }),
+    vim.keymap.set('n', '<c-h>', '<cmd>TmuxNavigateLeft<cr>', {
+      silent = true,
+      desc = 'Move to the left pane',
+    }),
+    vim.keymap.set('n', '<c-j>', '<cmd>TmuxNavigateDown<cr>', {
+      silent = true,
+      desc = 'Move to the lower pane',
+    }),
+    vim.keymap.set('n', '<c-k>', '<cmd>TmuxNavigateUp<cr>', {
+      silent = true,
+      desc = 'Move to the upper pane',
+    }),
+    vim.keymap.set('n', '<c-l>', '<cmd>TmuxNavigateRight<cr>', {
+      silent = true,
+      desc = 'Move to the right pane',
+    }),
   },
 }
+
 -- vim: ts=2 sts=2 sw=2 et
