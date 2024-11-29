de94444bd904385f8c7d8da6c018356f6ddcd0b9
Ian Howell <ian.howell0@gmail.com>
2024-11-29 17:07:22 -0600

    Delete some useless comments

--- links/nvim/init.lua
+++ links/nvim/init.lua
@@ -2,12 +2,10 @@
 --  - https://learnxinyminutes.com/docs/lua/
 --  - :help lua-guide (or HTML version): https://neovim.io/doc/user/lua-guide.html
 
--- Set <space> as the leader key
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
 require 'options'        -- ~/.config/nvim/lua/options.lua
@@ -22,5 +20,4 @@ require 'lazy-bootstrap' -- ~/.config/nvim/lua/lazy-bootstrap.lua
 
 require 'plugins'        -- ~/.config/nvim/lua/plugins.lua
 
--- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
