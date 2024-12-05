34f74df75418e11f6722c3ad587d3e71f3d96342
Ian Howell <ian.howell0@gmail.com>
2024-12-05 00:18:59 -0600

    Enable gitsigns plugin

--- links/nvim/lua/plugins.lua
+++ links/nvim/lua/plugins.lua
@@ -105,7 +105,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  -- TODO: Clean this up. For some bizarro reason, the kickstart authors split up the gitsigns plugin
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
