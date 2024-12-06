ed1b4b26c5392505f7160163079e689db59b78e2
Ian Howell <ian.howell0@gmail.com>
2024-12-06 14:31:48 -0600

    Formatting

--- links/nvim/init.lua
+++ links/nvim/init.lua
@@ -4,16 +4,16 @@
 
 vim.g.have_nerd_font = true
 
-require 'options'        -- ~/.config/nvim/lua/options.lua
+require 'options' -- ~/.config/nvim/lua/options.lua
 
-require 'keymaps'        -- ~/.config/nvim/lua/keymaps.lua
+require 'keymaps' -- ~/.config/nvim/lua/keymaps.lua
 
-require 'autocommands'   -- ~/.config/nvim/lua/autocommands.lua
+require 'autocommands' -- ~/.config/nvim/lua/autocommands.lua
 
-require 'focus'          -- ~/.config/nvim/lua/focus.lua
+require 'focus' -- ~/.config/nvim/lua/focus.lua
 
 require 'lazy-bootstrap' -- ~/.config/nvim/lua/lazy-bootstrap.lua
 
-require 'plugins'        -- ~/.config/nvim/lua/plugins.lua
+require 'plugins' -- ~/.config/nvim/lua/plugins.lua
 
 -- vim: ts=2 sts=2 sw=2 et
