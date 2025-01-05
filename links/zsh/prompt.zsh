c5cf0e7c2902cb655a7d0ca8e878d27c7ca3ef5c
Ian Howell <ian.howell0@gmail.com>
2025-01-05 09:39:31 -0600

    Move prompt config to prompt.zsh

--- links/zsh/prompt.zsh
+++ links/zsh/prompt.zsh
@@ -2,6 +2,13 @@
 # the delay when switching between viins and vicmd modes.
 KEYTIMEOUT=1
 
+# If ohmyposh is installed, use it to set the prompt. Note that this returns from the script early.
+# If oh-my-posh is not installed, skip this and use my custom prompt.
+if command -v oh-my-posh &> /dev/null; then
+  eval "$(oh-my-posh init zsh --config ~/.dotfiles/links/ohmyposh/tokyonight.omp.yaml)"
+  return
+fi
+
 function set_prompt() {
   # Return code must be done first
   return_code_icon="$(returncode_prompt_info $?)"
