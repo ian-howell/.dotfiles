40ffd33443c381f8b8acfe9c1e4dfd4c1c6f1739
Ian Howell <ian.howell0@gmail.com>
2025-01-12 12:54:57 -0600

    Add copilot toggles

--- links/nvim/lua/plugins/copilot.lua
+++ links/nvim/lua/plugins/copilot.lua
@@ -4,31 +4,40 @@ return {
     cmd = 'Copilot',
     event = 'InsertEnter',
 
-    opts = {
-      panel = { enabled = false },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        hide_during_completion = true,
-        debounce = 75,
-        keymap = {
-          -- This is the same as normal completion.
-          accept = '<C-y>',
-          accept_word = '<C-f>',
-          accept_line = '<C-l>',
-          -- We can't use <C-n> (or <C-p>) because those are the triggers for normal completion.
-          next = '<C-j>',
-          prev = '<C-k>',
-          -- This is the same as normal completion.
-          dismiss = '<C-e>',
+    config = function()
+      require('copilot').setup {
+        panel = { enabled = false },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          hide_during_completion = true,
+          debounce = 75,
+          keymap = {
+            -- This is the same as normal completion.
+            accept = '<C-y>',
+            accept_word = '<C-f>',
+            accept_line = '<C-l>',
+            -- We can't use <C-n> (or <C-p>) because those are the triggers for normal completion.
+            next = '<C-j>',
+            prev = '<C-k>',
+            -- This is the same as normal completion.
+            dismiss = '<C-e>',
+          },
         },
-      },
-      filetypes = {
-        ['*'] = true,
-      },
-      copilot_node_command = 'node', -- Node.js version must be > 18.x
-      server_opts_overrides = {},
-    },
+        filetypes = {
+          ['*'] = true,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+      }
+
+      -- Toggle
+      -- TODO: This just doesn't work. It doesn't even appear to work when I run the command directly. It
+      -- works more like 'Copilot disable'
+      -- vim.keymap.set('n', '<space>tc', '<cmd>Copilot toggle<CR>', { desc = 'Toggle Copilot' })
+      vim.keymap.set('n', '<space>tcd', '<cmd>Copilot disable<CR>', { desc = 'disable copilot' })
+      vim.keymap.set('n', '<space>tce', '<cmd>Copilot enable<CR>', { desc = 'enable copilot' })
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
