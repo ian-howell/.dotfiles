11075ce4c010c6a2cf1098ef151741f18f13630f
Ian Howell <ian.howell0@gmail.com>
2025-01-16 17:40:32 +0000

    Only autoformat lua files on save
    I'll have to adjust this in the future, but for now, it's making things
    really difficult at work.

--- links/nvim/lua/plugins/autoformat.lua
+++ links/nvim/lua/plugins/autoformat.lua
@@ -16,15 +16,12 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local enable_filetypes = { lua = true }
         local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
+        if enable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'fallback'
+        else
+          lsp_format_opt = 'never'
         end
         return {
           timeout_ms = 500,
