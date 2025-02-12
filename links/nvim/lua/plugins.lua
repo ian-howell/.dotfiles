a2de2af8504fe8e2e3c036d2e71ac7f93928dfed
Ian Howell <ian.howell0@gmail.com>
2025-02-12 23:27:11 +0000

    flit -> flash

--- links/nvim/lua/plugins.lua
+++ links/nvim/lua/plugins.lua
@@ -28,7 +28,7 @@ require('lazy').setup({
   require 'plugins/autopairs', -- ~/.config/nvim/lua/plugins/autopairs.lua
   require 'plugins/blink', -- ~/.config/nvim/lua/plugins/blink.lua'
   require 'plugins/copilot', -- ~/.config/nvim/lua/plugins/copilot.lua
-  require 'plugins/flit', -- ~/.config/nvim/lua/plugins/flit.lua
+  require 'plugins/flash', -- ~/.config/nvim/lua/plugins/flash.lua
   require 'plugins/fzf', -- ~/.config/nvim/lua/plugins/fzf.lua
   require 'plugins/gitsigns', -- ~/.config/nvim/lua/plugins/gitsigns.lua
   require 'plugins/go', -- ~/.config/nvim/lua/plugins/go.lua
