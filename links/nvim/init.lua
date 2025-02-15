cd1dc999e7bbb8d3dbdb1600a97a7929395bfdf2
Ian Howell <ian.howell0@gmail.com>
2025-02-15 23:01:51 +0000

    LazyVim

--- links/nvim/init.lua
+++ links/nvim/init.lua
@@ -1,21 +1,2 @@
--- Lua resources:
---  - https://learnxinyminutes.com/docs/lua/
---  - :help lua-guide (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-vim.g.have_nerd_font = true
-
-require 'options' -- ~/.config/nvim/lua/options.lua
-
-require 'keymaps' -- ~/.config/nvim/lua/keymaps.lua
-
-require 'autocommands' -- ~/.config/nvim/lua/autocommands.lua
-
-require 'focus' -- ~/.config/nvim/lua/focus.lua
-
-require 'lazy-bootstrap' -- ~/.config/nvim/lua/lazy-bootstrap.lua
-
-require 'plugins' -- ~/.config/nvim/lua/plugins.lua
-
-vim.cmd [[colorscheme tokyonight-storm]]
-
--- vim: ts=2 sts=2 sw=2 et
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
