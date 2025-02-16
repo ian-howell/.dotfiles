2e5849705ab77f1f1cd40e21e3cc9a6e878c2223
Ian Howell <ian.howell0@gmail.com>
2025-02-16 00:13:58 +0000

    Bring over my custom keymaps

--- links/nvim/lua/config/keymaps.lua
+++ links/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- See `:help vim.keymap.set()`
+
+-- Easy access to :split and :vsplit. Use '\' instead of '|' because of Shift.
+vim.keymap.set('n', '-', '<cmd>split<CR>', { desc = 'Split window horizontally' })
+vim.keymap.set('n', '\\', '<cmd>vsplit<CR>', { desc = 'Split window vertically' })
+
+-- Swap between buffers
+vim.keymap.set('n', '<space><space>', '<C-6>', { desc = '[ ] Swap to the last buffer' })
+
+-- vim: ts=2 sts=2 sw=2 et
