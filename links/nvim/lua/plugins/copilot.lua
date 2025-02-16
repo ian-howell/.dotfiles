4e0bb715a8bee4ca68789f2905a1e454021f7ed3
Ian Howell <ian.howell0@gmail.com>
2025-02-16 00:11:39 +0000

    Clean up copilot config

--- links/nvim/lua/plugins/copilot.lua
+++ links/nvim/lua/plugins/copilot.lua
@@ -1,32 +1,22 @@
 return {
-  { -- Copilot
+  {
     "zbirenbaum/copilot.lua",
 
     opts = {
-      -- suggestion = {
-      --   enabled = true,
-      --   auto_trigger = true,
-      --   hide_during_completion = true,
-      --   debounce = 75,
-      --   keymap = {
-      --     -- This is the same as normal completion.
-      --     accept = '<C-y>',
-      --     accept_word = '<C-f>',
-      --     accept_line = '<C-l>',
-      --     -- We can't use <C-n> (or <C-p>) because those are the triggers for normal completion.
-      --     next = '<C-j>',
-      --     prev = '<C-k>',
-      --     -- This is the same as normal completion.
-      --     dismiss = '<C-e>',
-      --   },
-      -- },
+      panel = { enabled = true },
+      suggestion = {
+        keymap = {
+          -- LazyVim's defaults here are actually pretty nice, since they more or less match vim defaults
+          accept_word = "<C-f>",
+          accept_line = "<C-l>",
+        },
+      },
       filetypes = {
         ["*"] = true,
       },
     },
 
     init = function()
-      -- Toggle
       -- TODO: This just doesn't work. It doesn't even appear to work when I run the command directly. It
       -- works more like 'Copilot disable'
       -- vim.keymap.set('n', '<space>tc', '<cmd>Copilot toggle<CR>', { desc = 'Toggle Copilot' })
@@ -34,6 +24,13 @@ return {
       vim.keymap.set("n", "<space>Cd", "<cmd>Copilot disable<CR>", { desc = "disable" })
       vim.keymap.set("n", "<space>Ce", "<cmd>Copilot enable<CR>", { desc = "enable" })
       vim.keymap.set("n", "<space>Cp", "<cmd>Copilot panel<CR>", { desc = "open panel" })
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    init = function()
+      vim.keymap.set("n", "<space>C", "", { desc = "+Copilot" })
+      vim.keymap.set({ "n", "x" }, "<space>Cc", "<cmd>CopilotChat<CR>", { desc = "Open Copilot Chat" })
     end,
   },
 }
