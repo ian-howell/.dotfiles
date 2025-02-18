61469fc24b060e48d08b9c08f32a030d6e738730
Ian Howell <ian.howell0@gmail.com>
2025-02-18 03:19:20 +0000

    Update ripple.nvim to use new config style

--- links/nvim/lua/plugins/ripple.lua
+++ links/nvim/lua/plugins/ripple.lua
@@ -8,14 +8,5 @@ return {
     "<C-Left>",
     "<C-Right>",
   },
-  opts = {
-    keys = {
-      -- TODO: The desc doesn't do anything right now. Figure out the "idiomatic" way to define keybinds for
-      -- plugins
-      { "<C-Up>", "expand_up", desc = "Increase window height (upwards)" },
-      { "<C-Down>", "expand_down", desc = "Increase window height (downwards)" },
-      { "<C-Left>", "expand_left", desc = "Increase window width (leftwards)" },
-      { "<C-Right>", "expand_right", desc = "Increase window width (rightwards)" },
-    },
-  },
+  opts = {},
 }
