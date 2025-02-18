0b5b1303a9a97e14f065c8115bfae10340db1ff1
Ian Howell <ian.howell0@gmail.com>
2025-02-18 03:20:04 +0000

    Format keymaps.lua

--- links/nvim/lua/config/keymaps.lua
+++ links/nvim/lua/config/keymaps.lua
@@ -1,10 +1,10 @@
 -- See `:help vim.keymap.set()`
 
 -- Easy access to :split and :vsplit. Use '\' instead of '|' because of Shift.
-vim.keymap.set('n', '-', '<cmd>split<CR>', { desc = 'Split window horizontally' })
-vim.keymap.set('n', '\\', '<cmd>vsplit<CR>', { desc = 'Split window vertically' })
+vim.keymap.set("n", "-", "<cmd>split<CR>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "\\", "<cmd>vsplit<CR>", { desc = "Split window vertically" })
 
 -- Swap between buffers
-vim.keymap.set('n', '<space><space>', '<C-6>', { desc = '[ ] Swap to the last buffer' })
+vim.keymap.set("n", "<space><space>", "<C-6>", { desc = "[ ] Swap to the last buffer" })
 
 -- vim: ts=2 sts=2 sw=2 et
