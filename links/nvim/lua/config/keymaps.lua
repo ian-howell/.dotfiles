160fa18146d8b1bc0cfe66bdaea2eac35ac151ff
Ian Howell <ian.howell0@gmail.com>
2025-03-02 17:06:17 +0000

    Add keymap to fix the last misspelling

--- links/nvim/lua/config/keymaps.lua
+++ links/nvim/lua/config/keymaps.lua
@@ -7,4 +7,7 @@ vim.keymap.set("n", "\\", "<cmd>vsplit<CR>", { desc = "Split window vertically" 
 -- Swap between buffers
 vim.keymap.set("n", "<space><space>", "<C-6>", { desc = "[ ] Swap to the last buffer" })
 
+-- Fix the last misspelling
+vim.keymap.set("i", "<C-s>", "<C-g>u<Esc>[s1z=`]a<C-g>u", { desc = "Fix the last mispelling" })
+
 -- vim: ts=2 sts=2 sw=2 et
