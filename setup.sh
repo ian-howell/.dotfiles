d7f2396655f0efe7586d2da5b11bf2e252295277
Ian Howell <ian.howell0@gmail.com>
2025-04-18 23:38:17 -0500

    Be smarted about finding files

--- setup.sh
+++ setup.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+base_dir="$(dirname "$(realpath "$0")")"
+
 prevent_apt_daemon_restart_prompts() {
     sudo sed -i "s/^#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 }
 
 install_components() {
     local components=(
-        "src/install_essentials.sh"
-        "src/install_go.sh"
-        "src/install_zsh.sh"
-        "src/install_fzf.sh"
-        "src/install_fd.sh"
-        "src/remap_capslock.sh"
-        "src/install_node.sh"
-        "src/install_delta.sh"
-        "src/install_lazygit.sh"
-        "src/install_neovim.sh"
-        "src/install_tmux.sh"
-        "src/install_zoxide.sh"
-        "src/install_sesh.sh"
-        "src/install_ohmyposh.sh"
+        "$base_dir/src/install_essentials.sh"
+        "$base_dir/src/install_go.sh"
+        "$base_dir/src/install_zsh.sh"
+        "$base_dir/src/install_fzf.sh"
+        "$base_dir/src/install_fd.sh"
+        "$base_dir/src/remap_capslock.sh"
+        "$base_dir/src/install_node.sh"
+        "$base_dir/src/install_delta.sh"
+        "$base_dir/src/install_lazygit.sh"
+        "$base_dir/src/install_neovim.sh"
+        "$base_dir/src/install_tmux.sh"
+        "$base_dir/src/install_zoxide.sh"
+        "$base_dir/src/install_sesh.sh"
+        "$base_dir/src/install_ohmyposh.sh"
     )
 
     for component in "${components[@]}"; do
@@ -30,7 +32,7 @@ install_components() {
 link_dotfiles() {
     # TODO: Just `go run` this. Currently, I need to rebuild the binary every time I
     # adjust my dotfile links.
-    ./src/linkdotfiles/linkdotfiles
+    ./$base_dir/src/linkdotfiles/linkdotfiles
 }
 
 update_git_submodules() {
