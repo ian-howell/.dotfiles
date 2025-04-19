d3b52aa098db9c779dc4133a9ef3cf4dc37910e2
Ian Howell <ian.howell0@gmail.com>
2025-04-18 23:55:44 -0500

    Formatting...

--- setup.sh
+++ setup.sh
@@ -3,48 +3,48 @@
 base_dir="$(dirname "$(realpath "$0")")"
 
 prevent_apt_daemon_restart_prompts() {
-    sudo sed -i "s/^#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+  sudo sed -i "s/^#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 }
 
 install_components() {
-    local components=(
-        "$base_dir/src/install_essentials.sh"
-        "$base_dir/src/install_go.sh"
-        "$base_dir/src/install_zsh.sh"
-        "$base_dir/src/install_fzf.sh"
-        "$base_dir/src/install_fd.sh"
-        "$base_dir/src/remap_capslock.sh"
-        "$base_dir/src/install_node.sh"
-        "$base_dir/src/install_delta.sh"
-        "$base_dir/src/install_lazygit.sh"
-        "$base_dir/src/install_neovim.sh"
-        "$base_dir/src/install_tmux.sh"
-        "$base_dir/src/install_zoxide.sh"
-        "$base_dir/src/install_sesh.sh"
-        "$base_dir/src/install_ohmyposh.sh"
-    )
+  local components=(
+    "$base_dir/src/install_essentials.sh"
+    "$base_dir/src/install_go.sh"
+    "$base_dir/src/install_zsh.sh"
+    "$base_dir/src/install_fzf.sh"
+    "$base_dir/src/install_fd.sh"
+    "$base_dir/src/remap_capslock.sh"
+    "$base_dir/src/install_node.sh"
+    "$base_dir/src/install_delta.sh"
+    "$base_dir/src/install_lazygit.sh"
+    "$base_dir/src/install_neovim.sh"
+    "$base_dir/src/install_tmux.sh"
+    "$base_dir/src/install_zoxide.sh"
+    "$base_dir/src/install_sesh.sh"
+    "$base_dir/src/install_ohmyposh.sh"
+  )
 
-    for component in "${components[@]}"; do
-        source "$component"
-    done
+  for component in "${components[@]}"; do
+    source "$component"
+  done
 }
 
 link_dotfiles() {
-    # TODO: Just `go run` this. Currently, I need to rebuild the binary every time I
-    # adjust my dotfile links.
-    ./$base_dir/src/linkdotfiles/linkdotfiles
+  # TODO: Just `go run` this. Currently, I need to rebuild the binary every time I
+  # adjust my dotfile links.
+  ./$base_dir/src/linkdotfiles/linkdotfiles
 }
 
 update_git_submodules() {
-    git submodule update --init --recursive
+  git submodule update --init --recursive
 }
 
 main() {
-    prevent_apt_daemon_restart_prompts
-    install_components
-    link_dotfiles
-    update_git_submodules
-    echo "========== COMPLETE =========="
+  prevent_apt_daemon_restart_prompts
+  install_components
+  link_dotfiles
+  update_git_submodules
+  echo "========== COMPLETE =========="
 }
 
 main
