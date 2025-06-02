aa4b0e4719160ee3f2dcfee036103e78add2c3a9
Ian Howell <ian.howell0@gmail.com>
2025-06-02 14:06:20 +0000

    Fix blink settings
    I guess folke fixed this...

--- links/nvim/lua/plugins/blink.lua
+++ links/nvim/lua/plugins/blink.lua
@@ -1,12 +1,6 @@
 return {
   "Saghen/blink.cmp",
   opts = {
-    cmdline = {
-      enabled = true,
-      keymap = {
-        preset = "enter",
-      },
-    },
     keymap = {
       preset = "default",
       ["<C-p>"] = { "show", "select_prev", "fallback_to_mappings" },
