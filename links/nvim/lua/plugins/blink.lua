cbed58b52c689dacaea6fa80192af239a8f5a14e
Ian Howell <ian.howell0@gmail.com>
2025-06-29 09:31:48 -0500

    Context-aware j/k mappings for blink.nvim

--- links/nvim/lua/plugins/blink.lua
+++ links/nvim/lua/plugins/blink.lua
@@ -3,8 +3,12 @@ return {
   opts = {
     keymap = {
       preset = "default",
-      ["<C-p>"] = { "show", "select_prev", "fallback_to_mappings" },
       ["<C-n>"] = { "show", "select_next", "fallback_to_mappings" },
+      ["<C-p>"] = { "show", "select_prev", "fallback_to_mappings" },
+
+      -- I love context aware ctrl+j/k mappings!
+      ["<C-j>"] = { "show", "select_next", "fallback_to_mappings" },
+      ["<C-k>"] = { "show", "select_prev", "fallback_to_mappings" },
     },
   },
 }
