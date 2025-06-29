4aa22dbadfa93441a1bff6f139ec5015a92fd6b4
Ian Howell <ian.howell0@gmail.com>
2025-06-29 12:25:04 -0500

    Fix flash highlights

--- links/nvim/lua/plugins/flash.lua
+++ links/nvim/lua/plugins/flash.lua
@@ -1,7 +1,34 @@
 return {
-  'folke/flash.nvim',
-  event = 'VeryLazy',
-  opts = {},
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    highlight = {
+      groups = {
+        label = "CustomFlashLabel",
+        current = "CustomFlashCurrent",
+        match = "CustomFlashMatch",
+      },
+    },
+  },
+  config = function(_, opts)
+    vim.api.nvim_set_hl(0, "CustomFlashLabel", {
+      fg = "#1a1b26", -- Dark background color for contrast
+      bg = "#7aa2f7", -- Tokyonight blue for visibility
+      bold = true,
+    })
+    vim.api.nvim_set_hl(0, "CustomFlashCurrent", {
+      fg = "#1a1b26", -- Dark text
+      bg = "#f7768e", -- Tokyonight red/pink
+      bold = true,
+    })
+
+    vim.api.nvim_set_hl(0, "CustomFlashMatch", {
+      fg = "#c0caf5", -- Light text
+      bg = "#414868", -- Darker blue-gray
+      bold = true,
+    })
+    require("flash").setup(opts)
+  end,
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
