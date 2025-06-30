f0aedab38c5bc7a4e22eafb186202b0e7472cdb3
Ian Howell <ian.howell0@gmail.com>
2025-06-29 23:19:55 -0500

    Prettier copilot

--- links/nvim/lua/plugins/copilot.lua
+++ links/nvim/lua/plugins/copilot.lua
@@ -107,7 +107,6 @@ return {
         callback = function()
           vim.opt_local.relativenumber = false
           vim.opt_local.number = false
-          vim.opt_local.conceallevel = 0
         end,
       })
 
