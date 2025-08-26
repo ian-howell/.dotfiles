4a2bd10305436186f4c50be265219046eb0ce1c4
Ian Howell <ian.howell0@gmail.com>
2025-08-26 09:49:56 -0500

    Fix H and L

--- links/nvim/lua/config/keymaps.lua
+++ links/nvim/lua/config/keymaps.lua
@@ -10,4 +10,9 @@ vim.keymap.set("n", "<space><space>", "<C-6>", { desc = "[ ] Swap to the last bu
 -- Fix the last misspelling
 vim.keymap.set("i", "<C-s>", "<C-g>u<Esc>[s1z=`]a<C-g>u", { desc = "Fix the last mispelling" })
 
+-- LazyVim uses H and L to move between buffers. Silly LazyVim, that's useless.
+-- This re-enables the much more useful defaults.
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+
 -- vim: ts=2 sts=2 sw=2 et
