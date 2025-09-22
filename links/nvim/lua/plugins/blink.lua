f5f39d7f50456b351f4a0bd2bff720c88f6c92e2
Ian Howell <ian.howell0@gmail.com>
2025-09-22 07:56:40 -0500

    Fix blink

--- links/nvim/lua/plugins/blink.lua
+++ links/nvim/lua/plugins/blink.lua
@@ -2,7 +2,14 @@ return {
   "Saghen/blink.cmp",
   opts = {
     cmdline = {
-      sources = { "inherit" },
+      keymap = {
+        preset = "cmdline",
+
+        -- I love context aware ctrl+j/k mappings!
+        ["<C-j>"] = { "show", "select_next", "fallback_to_mappings" },
+        ["<C-k>"] = { "show", "select_prev", "fallback_to_mappings" },
+      },
+      completion = { menu = { auto_show = true } },
     },
     keymap = {
       preset = "default",
