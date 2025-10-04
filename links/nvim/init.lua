06ae87859ee903e935a6384cf35721801ce6dbc5
Ian Howell <ian.howell0@gmail.com>
2025-10-04 15:23:10 -0500

    Fix some TODOs

--- links/nvim/init.lua
+++ links/nvim/init.lua
@@ -1,23 +1,27 @@
--- TODO: figure out how to create directory if it doesn't exist when saving
--- Check out how plugins do it
+-- Automatically create missing directories on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function(event)
+    local file = event.match
+    if file:match("^%w%w+://") then return end -- skip URIs
+    local dir = vim.fn.fnamemodify(file, ":p:h")
+    if dir ~= "" and vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
 
 -- require("config/focus.lua")
 
--- Fuzzy find files
-vim.keymap.set("n", "<space>ff", function() 
-	-- TODO: Call fzf and use output as an argument to :e
-end
-)
+-- TODO: Fuzzy find files
 
 -- Reload configuration
 vim.keymap.set("n", "<space>V", "<cmd>source $MYVIMRC<cr>")
 
 -- Open help
-vim.keymap.set("n", "<space>vh",":vert help ")
+vim.keymap.set("n", "<space>vh", ":vert help ")
 
 -- Open file explorer
 vim.keymap.set("n", "fe", "<cmd>15Lexplore<cr>")
-
 
 -- Window navigation
 vim.keymap.set({"n", "x"}, "<c-j>", "<c-w>j")
@@ -25,8 +29,9 @@ vim.keymap.set({"n", "x"}, "<c-k>", "<c-w>k")
 vim.keymap.set({"n", "x"}, "<c-h>", "<c-w>h")
 vim.keymap.set({"n", "x"}, "<c-l>", "<c-w>l")
 
--- Quickfix
+-- Undo / swap settings
+vim.o.undofile = true
+vim.o.swapfile = false
 
--- TODO: Fix these
--- vim.o.undo = true
--- vim.o.swap = false
+-- TODO: Figure this one out
+vim.o.botright = true
