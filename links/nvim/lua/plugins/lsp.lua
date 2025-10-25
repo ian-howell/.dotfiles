931c46eda88bd736e153640f3151d9a3dc46ada0
Ian Howell <ian.howell0@gmail.com>
2025-10-25 16:27:25 -0500

    Disable diagnostics for "improper" capitalization in Go LSP

--- links/nvim/lua/plugins/lsp.lua
+++ links/nvim/lua/plugins/lsp.lua
@@ -5,5 +5,22 @@ return {
       -- LazyVim turns this on by default, and it's really pretyy cluttered...
       enabled = false,
     },
+    diagnostics = {
+      virtual_text = {
+        source = "if_many",
+      },
+    },
+    servers = {
+      gopls = {
+        settings = {
+          gopls = {
+            analyses = {
+              -- Disable ST1003: naming convention checks
+              ST1003 = false,
+            },
+          },
+        },
+      },
+    },
   },
 }
