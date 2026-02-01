9b79bbbf8257737d1cfe6b9a60748806ebecdd14
Ian Howell <ian.howell0@gmail.com>
2026-01-31 21:19:36 -0600

    Pull over window splitting keybinds

--- links/nvim/lua/core/keymaps.lua
+++ links/nvim/lua/core/keymaps.lua
@@ -1,1 +1,6 @@
 -- Keymaps and related helper logic.
+-- See `:help vim.keymap.set()`
+
+-- Easy access to :split and :vsplit. Use '\' instead of '|' because of Shift.
+vim.keymap.set("n", "-", "<cmd>split<CR>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "\\", "<cmd>vsplit<CR>", { desc = "Split window vertically" })
