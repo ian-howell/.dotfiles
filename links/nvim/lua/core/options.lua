fc0f51d67169aeb06a65110d1edef9921deb099c
Ian Howell <ian.howell0@gmail.com>
2026-02-07 16:59:21 -0600

    show tabs

--- links/nvim/lua/core/options.lua
+++ links/nvim/lua/core/options.lua
@@ -44,7 +44,7 @@ vim.opt.splitkeep = "cursor"
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'` and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = "| ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
