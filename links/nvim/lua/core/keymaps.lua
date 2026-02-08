2d151b5f1061bf9ad94a3d09babcd0a1b149941a
Ian Howell <ian.howell0@gmail.com>
2026-02-07 19:31:31 -0600

    Add cross hair toggle

--- links/nvim/lua/core/keymaps.lua
+++ links/nvim/lua/core/keymaps.lua
@@ -4,3 +4,10 @@
 -- Easy access to :split and :vsplit. Use '\' instead of '|' because of Shift.
 vim.keymap.set("n", "-", "<cmd>split<CR>", { desc = "Split window horizontally" })
 vim.keymap.set("n", "\\", "<cmd>vsplit<CR>", { desc = "Split window vertically" })
+
+vim.g.cursorcolumn = vim.opt.cursorcolumn:get()
+vim.keymap.set("n", "<leader>ux", function()
+  vim.g.cursorcolumn = not vim.g.cursorcolumn
+  vim.opt.cursorcolumn = vim.g.cursorcolumn
+  vim.opt_local.cursorcolumn = vim.g.cursorcolumn
+end, { desc = "Toggle cursor column" })
