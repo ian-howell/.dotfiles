795f0dbe3ced55509dbd11883bc00c446434aa41
Ian Howell <ian.howell0@gmail.com>
2026-02-07 20:52:51 -0600

    snacks pickers for lsp

--- links/nvim/lua/lsp/init.lua
+++ links/nvim/lua/lsp/init.lua
@@ -17,26 +17,31 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local opts = { buffer = args.buf }
+    local has_snacks, snacks = pcall(require, "snacks")
 
     vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gi", snacks.picker.lsp_implementations, opts)
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
     vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
     vim.keymap.set("n", "<leader>wl", function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "<leader>f", function()
       vim.lsp.buf.format({ async = true })
     end, opts)
+
+    if has_snacks and snacks.picker then
+      vim.keymap.set("n", "gD", snacks.picker.lsp_declarations, opts)
+      vim.keymap.set("n", "gd", snacks.picker.lsp_definitions, opts)
+      vim.keymap.set("n", "gi", snacks.picker.lsp_implementations, opts)
+      vim.keymap.set("n", "<leader>D", snacks.picker.lsp_type_definitions, opts)
+      vim.keymap.set("n", "gr", snacks.picker.lsp_references, opts)
+    end
   end,
 })
 
