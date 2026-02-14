4ab1b00dd6965cd1b25394287d20cba29fbf5d2d
Ian Howell <ian.howell0@gmail.com>
2026-02-14 06:15:13 +0000

    Add GitHub CLI installer script

---
+++ src/install_gh.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ARTIFACTS_DIR="${ARTIFACTS_DIR:-$(mktemp -d)}"
+
+function main() {
+  latest_version=$(get_latest_version)
+  current_version=$(get_current_version)
+
+  if [[ "$latest_version" == "$current_version" ]]; then
+    return
+  fi
+
+  install_gh
+}
+
+function get_latest_version() {
+  url="https://api.github.com/repos/cli/cli/releases/latest"
+  curl -s "$url" | grep -Po '"tag_name": *"v\K[^"]*'
+}
+
+function get_current_version() {
+  if ! command -v gh >/dev/null; then
+    return
+  fi
+  gh --version | head -1 | awk '{print $3}'
+}
+
+function install_gh() {
+  cd "$ARTIFACTS_DIR" || return
+
+  local url
+  url+="https://github.com/cli/cli/"
+  url+="releases/download/v${latest_version}/"
+  url+="gh_${latest_version}_linux_amd64.tar.gz"
+  curl -sLo gh.tar.gz "$url"
+  tar xf gh.tar.gz gh_${latest_version}_linux_amd64
+  sudo install gh_${latest_version}_linux_amd64/bin/gh -D -t /usr/local/bin/
+}
+
+main
