ec84412871c2effaf4251787016961e43cb89abe
Ian Howell <ian.howell0@gmail.com>
2026-02-21 13:13:51 -0600

    Turn on wrap by default

--- links/nvim/lua/core/options.lua
+++ links/nvim/lua/core/options.lua
@@ -16,8 +16,9 @@ vim.opt.showmode = false
 -- Enable break indent
 vim.opt.breakindent = true
 
--- Don't wrap lines
-vim.opt.wrap = false
+-- Wrap lines. This is needed because copilot's suggestions can be very long, and they force the
+-- screen to jump around if wrap is disabled.
+vim.opt.wrap = true
 
 -- Save undo history
 vim.opt.undofile = true
