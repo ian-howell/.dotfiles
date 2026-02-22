1bf8ad146f1a55beeb9460ef71bb5b843d519574
Ian Howell <ian.howell0@gmail.com>
2026-02-22 09:17:57 -0600

    Make escape close floating windows in Neovim LSP

--- links/nvim/lua/lsp/init.lua
+++ links/nvim/lua/lsp/init.lua
@@ -9,5 +9,13 @@ vim.diagnostic.config({
   float = { border = "rounded", source = "if_many" },
 })
 
+-- Map escape to close floating windows, like diagnostics and hover previews
+local open_floating_preview = vim.lsp.util.open_floating_preview
+vim.lsp.util.open_floating_preview = function(contents, syntax, opts, ...)
+  local bufnr, winid = open_floating_preview(contents, syntax, opts, ...)
+  vim.keymap.set("n", "<Esc>", "<cmd>close<CR>", { buffer = bufnr })
+  return bufnr, winid
+end
+
 give("lsp.servers.gopls")
 give("lsp.servers.bashls")
