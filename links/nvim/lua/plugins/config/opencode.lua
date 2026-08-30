b79c03cc6a6780fdb42e1db8fba832a11268898a
Ian Howell <ian.howell0@gmail.com>
2026-08-29 21:17:19 -0500

    nvim: add OpenCode session mappings

--- links/nvim/lua/plugins/config/opencode.lua
+++ links/nvim/lua/plugins/config/opencode.lua
@@ -31,3 +31,18 @@ end, { desc = "Ask OpenCode about this" })
 vim.keymap.set({ "n", "x" }, "<leader>om", function()
   opencode.select()
 end, { desc = "OpenCode menu" })
+
+local function map_command(key, command, desc)
+  vim.keymap.set("n", key, function()
+    opencode.command(command)
+  end, { desc = desc })
+end
+
+map_command("<leader>os", "session.select", "Select OpenCode session")
+map_command("<leader>on", "session.new", "New OpenCode session")
+map_command("<leader>oi", "session.interrupt", "Interrupt OpenCode session")
+map_command("<leader>ou", "session.undo", "Undo OpenCode action")
+map_command("<leader>or", "session.redo", "Redo OpenCode action")
+map_command("<leader>og", "session.first", "First OpenCode message")
+map_command("<leader>oG", "session.last", "Last OpenCode message")
+map_command("<leader>o<Tab>", "agent.cycle", "Cycle OpenCode agent")
